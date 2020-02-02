ea52a0aeeb7490d1966f3cf4ab34f9a7eb6bbd63
Elena Puggioni <elepuggioni@tiscali.it>
2020-02-02 16:05:30 +0100

    //initial commit

---
+++ app/src/main/java/com/example/progettoium/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.progettoium;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //Intent goToLogin = new Intent(getApplicationContext(), login.class);
+    }
+}
