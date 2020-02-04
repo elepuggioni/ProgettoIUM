86c56644119a13c7c99c82cd67ca134eba3dff5b
sarareg0412 <sara98.regali@gmail.com>
2020-02-04 13:25:37 +0100

    Fatta Register

--- app/src/main/java/com/example/progettoium/MainActivity.java
+++ app/src/main/java/com/example/progettoium/MainActivity.java
@@ -6,22 +6,32 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     Button provamaps;
+    TextView register;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         provamaps = findViewById(R.id.provamaps);
-
+        register= findViewById(R.id.register);
         provamaps.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent showHome = new Intent(MainActivity.this, Home2.class);
                 startActivity(showHome);
+            }
+        });
+
+        register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent showRegister = new Intent(MainActivity.this, Register.class);
+                startActivity(showRegister);
             }
         });
     }
